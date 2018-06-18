a29289f895bd31c7912aff348a4ad2a8e9ab89e1
Carl <carlp@grindrodbank.co.za>
2018-06-18 08:06:58 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lowerBound = Environment.GetEnvironmentVariable("lowerBound") == null ? 1 : Convert.ToInt32(Environment.GetEnvironmentVariable("lowerBound"));
+            var upperBound = Environment.GetEnvironmentVariable("upperBound") == null ? 100 : Convert.ToInt32(Environment.GetEnvironmentVariable("upperBound"));
+
+            var fizzAt = Environment.GetEnvironmentVariable("fizzAt") == null ? 3 : Convert.ToInt32(Environment.GetEnvironmentVariable("fizzAt"));
+            var buzzAt = Environment.GetEnvironmentVariable("buzzAt") == null ? 3 : Convert.ToInt32(Environment.GetEnvironmentVariable("buzzAt"));
+
+            var outPut = "";
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (i % fizzAt == 0)
+                    outPut = "FIZZ";
+
+                if (i % buzzAt == 0)
+                    outPut += "BUZZ";
+
+                if (outPut.Equals(""))
+                    Console.WriteLine(i);
+                else
+                    Console.WriteLine(outPut);
+
+                outPut = "";
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# What is FizzBuzz?
+
+A popular interview question is the "FizzBuzz test", its based on a children's game where you count from 1 to 100 and for multiples of 3 say "Fizz", multiples of 5 say "Buzz", if both say "FizzBuzz" else say the number, example 8.
+
+# Translated into to code domain the question you could as is:
+
+"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
