911fec4210df100e84b49971f160d361addd5728
Carl <carlp@grindrodbank.co.za>
2018-06-18 08:12:37 +0200

    updated Dockerfile and shell script

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,1 +1,9 @@
-﻿
+﻿#!/bin/bash
+docker pull carlpaton/fizzbuzz
+sudo docker container kill fizzbuzz
+sudo docker rm fizzbuzz
+
+sudo docker run --env-file=env_file_name.env -d -p 81:80 --name fizzbuzz carlpaton/fizzbuzz
+
+sudo docker start fizzbuzz
+sudo docker ps --all
